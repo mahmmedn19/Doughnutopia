27ba52a0326028d16563a98970816c5c01baf0f3
Mohamed Naser <mahmmedn19@gmail.com>
2023-07-12 16:47:10 +0300

    implement some composable

--- app/src/main/java/com/example/doughnutopia/screens/home/Home.kt
+++ app/src/main/java/com/example/doughnutopia/screens/home/Home.kt
@@ -6,3 +6,32 @@
 
 package com.example.doughnutopia.screens.home
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.doughnutopia.screens.home.composable.DonutsText
+import com.example.doughnutopia.screens.home.composable.GonutsWithSearch
+import com.example.doughnutopia.screens.home.composable.TodayOffers
+
+@Composable
+fun HomeScreen() {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(horizontal = 16.dp)
+    ) {
+        GonutsWithSearch()
+        TodayOffers()
+        DonutsText()
+    }
+}
+
+@Preview(showSystemUi = true, showBackground = true)
+@Composable
+fun PreviewScreen() {
+    HomeScreen()
+}
---
+++ app/src/main/java/com/example/doughnutopia/screens/home/composable/GonutsWithSearchIcon.kt
@@ -0,0 +1,51 @@
+/*
+ * Created by Mohamed Naser on 7/12/23, 1:06 PM, 2023.
+ *
+ * Copyright (c) 2023 All rights reserved.
+ */
+
+package com.example.doughnutopia.screens.home.composable
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.example.doughnutopia.R
+import com.example.doughnutopia.composable.CustomIcon
+import com.example.doughnutopia.composable.CustomText
+import com.example.doughnutopia.ui.theme.Pink_20
+import com.example.doughnutopia.ui.theme.Typography
+@Composable
+fun GonutsWithSearch(){
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(top = 32.dp),
+        horizontalArrangement = Arrangement.SpaceBetween
+    ) {
+        Column {
+            CustomText(
+                text = "Let’s Gonuts!",
+                style = Typography.titleLarge
+            )
+            CustomText(
+                text = "Order your favourite donuts from here",
+                style = Typography.labelMedium
+            )
+        }
+        CustomIcon(
+            modifier = Modifier
+                .background(
+                    Pink_20, RoundedCornerShape(8.dp)
+                )
+                .padding(4.dp),
+            drawableResId = R.drawable.ic_search,
+        )
+    }
+}
