f11077e6a4f47f33a2ef447c8c6b69f972097523
Mohamed Naser <mahmmedn19@gmail.com>
2023-07-12 10:02:39 +0300

    add necessary files and pkg

---
+++ app/src/main/java/com/example/doughnutopia/screens/details/Details.kt
@@ -0,0 +1,8 @@
+/*
+ * Created by Mohamed Naser on 7/12/23, 9:01 AM, 2023.
+ *
+ * Copyright (c) 2023 All rights reserved.
+ */
+
+package com.example.doughnutopia.screens.details
+
---
+++ app/src/main/java/com/example/doughnutopia/screens/onboarding/OnBoarding.kt
@@ -0,0 +1,8 @@
+/*
+ * Created by Mohamed Naser on 7/12/23, 9:02 AM, 2023.
+ *
+ * Copyright (c) 2023 All rights reserved.
+ */
+
+package com.example.doughnutopia.screens.onboarding
+
